e69289489bf50425f08f6e324913bfa4f0fe49ba
astrobuddha <jozefleo@protonmail.com>
2023-05-14 18:17:24 -0400

    start on the pastebin part

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,18 @@ extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "
+    USAGE
+
+      POST /
+
+          accepts raw data in the body of the request and responds with a URL of
+          a page containing the body's content
+
+      GET /<id>
+
+          retrieves the content for the paste with id `<id>`
+    "
 }
 
 #[launch]
---
+++ src/paste_id.rs
@@ -0,0 +1,17 @@
+use std::borrow::Cow;
+use std::path::{Path, PathBuf};
+
+// a _Probably_ unique paste ID.
+pub struct PasteId<'a>(Cow<'a, str>);
+
+impl PasteId<'_> {
+    /// Generate a _probably_ unique ID with `size` characters. For readability,
+    /// the characters used are from the sets [0-9], [A-Z], [a-z]. The
+    /// probability of a collision depends on the value of `size` and the number
+    /// of IDs generated thus far.
+    pub fn new(size: usize) -> PasteId<'static> {
+        const BASE62: &[u8] = b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+        let mut id = String::with_capacity(size);
+    }
+}
